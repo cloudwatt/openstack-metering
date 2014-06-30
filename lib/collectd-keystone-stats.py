e1e84b9b7e1cb624c327b1cc7b0c3856b6353d4b
sathlan <chem@sathlan.org>
2014-06-30 10:53:38 -0400

    Adding basic keystone metrics.

---
+++ lib/collectd-keystone-stats.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Collectd plugin for graphing nova hypervisor-stats
+#
+# Copyright © 2012-2014 eNovance <licensing@enovance.com>
+#
+# Authors:
+#   Sofer Athlan-Guyot <sofer.athlan@enovance.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Requirments:  python-keystoneclient, collectd
+import collectd
+
+from keystoneclient.v2_0 import client
+from datetime import datetime
+from time import mktime
+from pprint import pformat
+
+
+plugin_name = 'collectd-keystone-stats'
+
+# NOTE: This version is grepped from the Makefile, so don't change the
+# format of this line.
+version = '0.1.0'
+
+config = {
+    'endpoint_type': "publicURL",
+    'verbose_logging': False,
+    'error': None,
+    'volume_type': None,
+    'metric_name': 'openstack.keystone.stats',
+}
+
+class Novautils:
+    def __init__(self, nova_client):
+        self.nova_client = nova_client
+        self.last_stats = None
+        self.connection_done = None
+        self.stats = {}
+
+    def check_connection(self, force=False):
+        if not self.connection_done or force:
+            try:
+                # force a connection to the server
+                self.connection_done = self.nova_client.authenticate()
+            except Exception as e:
+                log_error("Cannot connect to cinder: %s\n" % e)
+
+    def get_stats(self):
+        stats = {}
+        self.last_stats = int(mktime(datetime.now().timetuple()))
+        #TODO: check what happens when pagination is activated.
+        stats['users'] = len(self.nova_client.users.list())
+        stats['tenants'] = len(self.nova_client.tenants.list())
+
+        return stats
+
+
+def log_verbose(msg):
+    if not config['verbose_logging']:
+        return
+    collectd.info("%s [verbose]: %s" % (plugin_name, msg))
+
+def log_warning(msg):
+    collectd.warning("%s [warning]: %s" % (plugin_name, msg))
+    
+def log_error(msg):
+    global config
+    error = "%s [error]: %s" % (plugin_name, msg)
+    collectd.error(error)
+    config['error'] = error
+
+def dispatch_value(key, value, type, metric_name, date=None, type_instance=None):
+    """Dispatch a value"""
+
+    if not type_instance:
+        type_instance = key
+
+    value = int(value)
+    log_verbose('Sending value: %s=%s' % (type_instance, value))
+
+    val = collectd.Values(plugin=metric_name)
+    val.type = type
+    if date:
+        val.time = date
+    val.type_instance = type_instance
+    val.values = [value]
+    val.dispatch()
+
+def configure_callback(conf):
+    """Receive configuration block"""
+    global config
+    for node in conf.children:
+        if node.key == 'AuthURL':
+            config['auth_url'] = node.values[0]
+        elif node.key == 'Username':
+            config['username'] = node.values[0]
+        elif node.key == 'Password':
+            config['password'] = node.values[0]
+        elif node.key == 'Tenant':
+            config['tenant'] = node.values[0]
+        elif node.key == 'EndpointType':
+            config['endpoint_type'] = node.values[0]
+        elif node.key == 'Verbose':
+            config['verbose_logging'] = bool(node.values[0])
+        elif node.key == 'MetricName':
+            config['metric_name'] = node.values[0]
+        else:
+            collectd.warning('nova_hypervisor_stats_info plugin: Unknown config key: %s.'
+                             % node.key)
+    if not config.has_key('auth_url'):
+        log_error('AuthURL not defined')
+
+    if not config.has_key('username'):
+        log_error('Username not defined')
+
+    if not config.has_key('password'):
+        log_error('Password not defined')
+
+    if not config.has_key('tenant'):
+        log_error('Tenant not defined')
+
+    log_verbose('Configured with auth_url=%s, username=%s, password=%s, tenant=%s, endpoint_type=%s, metric_name=%s' % (config['auth_url'], config['username'], config['password'], config['tenant'], config['endpoint_type'], config['metric_name']))
+
+def connect(config):
+    try:
+        nova_client = client.Client(
+            username=config['username'],
+            tenant_name=config['tenant'],
+            password=config['password'],
+            auth_url=config['auth_url'],
+        )
+        nova_client.authenticate()
+        if not nova_client.tenants.list():
+            log_error("The user must have the admin role to work.")
+    except Exception as e:
+        log_error("Authentication error: %s\n" % e)
+    return nova_client
+
+def init_callback():
+    """Initialization block"""
+    global config
+    client = connect(config)
+    config['util'] = Novautils(client)
+    config['util'].check_connection()
+    log_verbose('Got a valid connection to keystone API')
+
+def read_callback(data=None):
+    if config['error']:
+        log_warning("Got a error during initialization.  Fix it and restart collectd")
+        return
+    if not config.has_key('util'):
+        if not config['util'].connection_done:
+            log_warning("Connection has not been done. Exiting.")
+            return
+    info = config['util'].get_stats()
+    log_verbose(pformat(info))
+    for key in info:
+        dispatch_value(key, info[key], 'gauge', config['metric_name'], config['util'].last_stats)
+
+collectd.register_config(configure_callback)
+collectd.register_init(init_callback)
+collectd.register_read(read_callback)
+                
