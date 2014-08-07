2ced92146401d07553a652f1fb703ebbe12edc87
Sebastien Badia <seb@sebian.fr>
2014-08-07 23:44:00 +0200

    ccli: Added missing GPLv3 header

--- bin/collectd-cli.py
+++ bin/collectd-cli.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
-
+# -*- encoding: utf-8 -*-
+#
+# Collectd command line interface
+#
+# A litle utility is given to run the plugin on the command line in the bin
+# directory. To use it give the collectd script as argument and some other
+# required parameters.
+#
+# Copyright © 2014 eNovance <licensing@enovance.com>
+#
+# Authors:
+#   Sofer Athlan-Guyot <sofer.athlan@enovance.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 import argparse
 import datetime
 
